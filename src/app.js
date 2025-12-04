46ea6a2751b6e7daea49c870994147dab341e7dc
AgustinMGauto <agustin_gauto@outlook.com>
2025-12-04 12:44:16 +0000

    boton que cambia carta

--- src/app.js
+++ src/app.js
@@ -3,16 +3,18 @@
 let Numeros = [2,3,4,5,6,7,8,9,10,"J","K","Q","A"]
 let Simbolos = ["♦","♥","♠","♣"]
 
+// window.onload = function() {
+    window.myClickFunction = function myClickFunction() {
+  
 
 function SelecionAuto (num) {
     const numal = Math.floor(Math.random()*num.length)
     return num[numal]
 }
 
-    const numeroCarta = SelecionAuto(Numeros)
-    const SimboloCarta = SelecionAuto(Simbolos)
 
-    
+const numeroCarta = SelecionAuto(Numeros)
+const SimboloCarta = SelecionAuto(Simbolos)
 
 const elementsimboloAR = document.getElementById("icono")
 elementsimboloAR.innerHTML = SimboloCarta
@@ -23,5 +25,9 @@ elementsimboloAB.innerHTML = SimboloCarta
 const elementNumero = document.getElementById("numero")
 elementNumero.innerHTML = numeroCarta
 
-SimboloCarta === "♦" || "♥" 
+const clasecard = document.querySelector(".card")
 
+if (SimboloCarta === "♦" || SimboloCarta === "♥"){clasecard.classList.add("rojo")} else {clasecard.classList.remove("rojo")}
+
+
+};
