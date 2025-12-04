fa9a6626820697fb3edbcc039205b81f44189e9d
AgustinMGauto <agustin_gauto@outlook.com>
2025-12-04 13:13:45 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 
 let Numeros = [2,3,4,5,6,7,8,9,10,"J","K","Q","A"]
-let Simbolos = ["♦",""♥"","♠","♣"]
+let Simbolos = ["♦","♥","♠","♣"]
 
 // window.onload = function() {
     window.myClickFunction = function myClickFunction() {
